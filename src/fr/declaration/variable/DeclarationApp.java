21ec7f49376340e65d301ad5de855792af6b4fb5
will <djabel.willyu@gmail.com>
2024-10-02 15:05:18 +0200

    add DeclarationApp.class

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        byte b = 1;
+        short s = 1200;
+        int i = 20000000;
+        long l = 30000000000000000L;
+        float f = 2.1F;
+        double d = 3.4;
+        char c = 'c';
+        String t = "hello";
+        boolean z = true;
+        String randomString = "Voici le resultat d’un calcul : 1+5=6";
+
+        System.out.println(b + "\n" + s + "\n" + i + l + "\n" + f + "\n" + d + c + "\n" + t + "\n" + z );
+        System.out.println(randomString);
+    }
+}
